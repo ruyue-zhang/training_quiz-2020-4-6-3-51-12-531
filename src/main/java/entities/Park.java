0c1e94ecbe9b73766facded0dff507fd3bdf3d9c
lpdft <772346730@qq.com>
2020-04-07 13:06:24 +0800

    [ruyue] feat: add entities package and add Park Class

---
+++ src/main/java/entities/Park.java
@@ -0,0 +1,49 @@
+package entities;
+
+public class Park {
+    private int id;
+    private String ParkingSpace;
+    private String licenseNumber;
+
+    public Park() {
+    }
+
+    public Park(int id, String parkingSpace, String licenseNumber) {
+        this.id = id;
+        ParkingSpace = parkingSpace;
+        this.licenseNumber = licenseNumber;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getParkingSpace() {
+        return ParkingSpace;
+    }
+
+    public void setParkingSpace(String parkingSpace) {
+        ParkingSpace = parkingSpace;
+    }
+
+    public String getLicenseNumber() {
+        return licenseNumber;
+    }
+
+    public void setLicenseNumber(String licenseNumber) {
+        this.licenseNumber = licenseNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Park{" +
+                "id=" + id +
+                ", ParkingSpace='" + ParkingSpace + '\'' +
+                ", licenseNumber='" + licenseNumber + '\'' +
+                '}';
+    }
+}
