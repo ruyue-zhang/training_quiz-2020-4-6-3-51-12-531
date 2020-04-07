e6fef3c01482c1a8bb3cd8e71bfaae9d06c76b49
lpdft <772346730@qq.com>
2020-04-07 11:49:10 +0800

    [ruyue] feat: add BaseDAO class and finish query one function

---
+++ src/main/java/BaseDAO.java
@@ -0,0 +1,51 @@
+import Utils.JDBCUtils;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+
+public class BaseDAO<T> {
+    private Class<T> clazz = null;
+
+    {
+        Type genericSuperclass = this.getClass().getGenericSuperclass();
+        ParameterizedType paramType = (ParameterizedType) genericSuperclass;
+        Type[] actualTypeArguments = paramType.getActualTypeArguments();
+        clazz = (Class<T>) actualTypeArguments[0];
+    }
+
+    public T query(Connection conn, String sql, Object...args) {
+        PreparedStatement pstm = null;
+        ResultSet result = null;
+
+        try {
+            pstm = conn.prepareStatement(sql);
+            for (int i = 0; i < args.length; i++) {
+                pstm.setObject(i + 1,args[i]);
+            }
+            result = pstm.executeQuery();
+            ResultSetMetaData metaData = result.getMetaData();
+            int columnCount = metaData.getColumnCount();
+            if(result.next()) {
+                T t = clazz.newInstance();
+                for (int i = 0; i < columnCount; i++) {
+                    Object columnValue = result.getObject(i + 1);
+                    String columnLabel = metaData.getColumnLabel(i + 1);
+                    Field declaredField = clazz.getDeclaredField(columnLabel);
+                    declaredField.setAccessible(true);
+                    declaredField.set(t, columnValue);
+                }
+                return t;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtils.releaseSource(result, pstm, null);
+        }
+        return null;
+    }
+}
