3a95c07abb902429fddb5b0620cd9e8d15270e8e
lpdft <772346730@qq.com>
2020-04-07 14:01:19 +0800

    [ruyue] feat: finish updateLicenseNumber and selectByLicenseNumber function

--- src/main/java/repositories/ParkRepository.java
+++ src/main/java/repositories/ParkRepository.java
@@ -15,12 +15,14 @@ public class ParkRepository extends BaseDAO<Park> implements ParkRepositoryI  {
 
     @Override
     public int updateLicenseNumber(Connection conn, String licenseNumber) {
-        return 0;
+        String sql = "update park set license_number = ? where license_number is null";
+        return update(conn,licenseNumber);
     }
 
     @Override
     public Park selectByLicenseNumber(Connection conn, String licenseNumber) {
-        return null;
+        String sql = "select (id,parking_space,license_number) from park where license_number = licenseNumber";
+        return query(conn,sql);
     }
 
     @Override
