04c0b951a7c2e729f7a5a05fa3d956ce27e2b75b
lpdft <772346730@qq.com>
2020-04-07 18:29:12 +0800

    [ruyue] feat: add controllers package and parkController

---
+++ src/main/java/Controllers/parkController.java
@@ -0,0 +1,17 @@
+package Controllers;
+
+import services.parkService;
+
+public class parkController {
+    public static void init(String initInfo) {
+
+    }
+
+    public static String park(String carNumber) {
+        return "";
+    }
+
+    public static String fetch(String ticket) {
+        return "";
+    }
+}
