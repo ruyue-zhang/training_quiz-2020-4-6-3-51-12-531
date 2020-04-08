585de54ee39f729ddd423ed098532060e92cf509
lpdft <772346730@qq.com>
2020-04-08 12:50:37 +0800

    [ruyue] feat: fetch success change license_number is null

--- src/main/java/services/ParkService.java
+++ src/main/java/services/ParkService.java
@@ -68,6 +68,7 @@ public class ParkService implements ParkServiceI {
             if(returnPark == null) {
                 throw new InvalidTicketException("很抱歉，无法通过您提供的停车券为您找到相应的车辆，请您再次核对停车券是否有效！ ");
             }
+            parkRepository.emptyLicenseNumberByIdAndParkingSpace(conn,returnPark.getId(),returnPark.getParkingSpace());
             return returnPark.getLicenseNumber();
         } catch (SQLException e) {
             e.printStackTrace();
